82bccf00efa203021470ce3ef0da2b6666550c6a
vikrantgupta25 <vikrant.thomso@gmail.com>
2024-10-07 02:02:22 +0530

    setup the ASTPrinter sync

--- app/printer.js
+++ app/printer.js
@@ -1,8 +1,16 @@
-import "./expression.js";
-
 export class Visitor {
-  accept() {
-    console.log("visitor not implemented for this expression class!");
+  // for every new visitor we need to define the below 4 functions to map them correctly to their classes
+  visitBinaryExpression() {
+    console.log("visit binary expression missing for the visitor");
+  }
+  visitUnaryExpression() {
+    console.log("visit unary expression missing for the visitor");
+  }
+  visitGroupingExpression() {
+    console.log("visit grouping expression missing for the visitor");
+  }
+  visitLiteralExpression() {
+    console.log("visit literal expression missing for the visitor");
   }
 }
 
@@ -11,13 +19,25 @@ export class ASTPrinter extends Visitor {
     expression.accept(this);
   }
 
-  visitBinaryExpression(expression) {
-    console.log(expression, "visited from binary expression");
+  parenthesize(...exprs) {
+    // todo[vikrantgupta25]: define the proper parenthesize function here to complete the ASTPrinter Visitor
+    console.log(exprs);
   }
 
-  visitUnaryExpression() {}
+  visitBinaryExpression(expr) {
+    this.parenthesize(expr.operator.lexeme, expr.left, expr.right);
+  }
 
-  visitGroupingExpression() {}
+  visitUnaryExpression() {
+    this.parenthesize(expr.operator.lexeme, expr.right);
+  }
 
-  visitLiteralExpression() {}
+  visitGroupingExpression(expr) {
+    this.parenthesize("group", expr.expression);
+  }
+
+  visitLiteralExpression() {
+    if (expr.value == null) return "nil";
+    return expr.value.toString();
+  }
 }
